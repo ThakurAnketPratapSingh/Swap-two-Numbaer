650f7e6ad0a74f0978da83469f683e0885e2efbe
Anket pratap singh <72145331+ThakurAnketPratapSingh@users.noreply.github.com>
2022-04-10 17:31:30 +0530

    Add files via upload

---
+++ test/index.js
@@ -0,0 +1,10 @@
+let a = prompt('Enter the first variable: ');
+let b = prompt('Enter the second variable: ');
+let temp;
+temp = a;
+a = b;
+b = temp;
+alert(`The value of A after swapping: ${a}
+    The value of B after swapping: ${b}`);
+console.log(`The value of A after swapping: ${a}`);
+console.log(`The value of B after swapping: ${b}`);
